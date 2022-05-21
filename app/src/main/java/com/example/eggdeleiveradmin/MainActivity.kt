5d5ed8a8a1979332367495095d66d00e5ce5b4dc
msi_PC <babdiyev361@gmail.com>
2022-05-21 12:58:27 +0500

    Initial commit

---
+++ app/src/main/java/com/example/eggdeleiveradmin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.eggdeleiveradmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        setContentView(R.layout.activity_main)
+    }
+}
